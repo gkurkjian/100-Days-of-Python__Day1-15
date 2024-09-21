213601799deffdd04498f211e4bbb352ed63f94d
George K <gkurkjian@myseneca.ca>
2024-09-20 21:54:13 -0400

    first practive of for loop

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,23 @@
 fruits = ["Apple", "Peach", "Pear"]
+
+# for fruit in fruits:  ## basically it's assigning var = fruit and iterating each item in List
+#     print(fruit)  ## The output will be;  Apple
+#                                         # Peach
+#                                         # Pear
+#                                         # Apple
+#
+
+
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " Pie")   # Apple
+#                             # Apple Pie
+#                             # Peach
+#                             # Peach Pie
+#                             # Pear
+#                             # Pear Pie
+
+for fruit in fruits:  ## The indentation it's important. Now it'll print the List on line 23
+    print(fruit)
+    print(fruit + " Pie")
+    print(fruits)
