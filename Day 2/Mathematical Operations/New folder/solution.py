bb6440745b33f92a297050c76c2d757c46e51443
George K <gkurkjian@myseneca.ca>
2024-10-09 21:26:43 -0400

    Finishin mid-way through/ quiz

--- Day 2/Mathematical Operations/New folder/solution.py
+++ Day 2/Mathematical Operations/New folder/solution.py
@@ -1,25 +1,35 @@
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
-bill = 0
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-if height >= 120:
-    print("You can ride the rollercoaster")
-    age = int(input("What is your age? "))
-    if age <= 12:
-        bill = 5
-        print("Please pay $5.")
-    elif age <= 18:
-        bill = 7
-        print("Please pay $7.")
-    else:
-        bill  = 12
-        print("Please pay $12.")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
-    wants_photo = input("Do you want to have a photo take? Type y or Yes and n for No.")
-    if wants_photo == "y":
-        #Add $3 to the bill
-        bill += 3
 
-        print(f"Your final bill is {bill}")
-else:
-    print("Sorry you have to grow taller before you can ride.")
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+
+# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+#  by the shift amount and print the encrypted text.
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+
+    ## abc 2
+    for letter in original_text:
+
+        shifted_position = alphabet.index(letter) + shift_amount  ## "a" it's on position 0, we wanted on position 2 which is #c
+        shifted_position = shifted_position % len(alphabet)  ##In case user want to shift from letter 'z',
+
+        #or shifted_position %= len(alphabet)
+
+        cipher_text += alphabet[shifted_position]  ## here we got letter #c
+
+    print(f"Here is encoded result: {cipher_text}")
+
+# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+
+
+
+# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
+#  message.
+
+encrypt(original_text=text, shift_amount=shift)
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,28 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+from xml.dom.minidom import ProcessingInstruction
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+print(programming_dictionary["Bug"])
+
+
+my_empty_dictionary = {}  ## creating an empty dictionary
+print(type(my_empty_dictionary))  ## <class 'dict'>
+
+## Adding new value into the dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+print(programming_dictionary["Loop"])
+
+## Edit the item in the dictionary
+programming_dictionary["Bug"] = "Kaka"
+
+print(programming_dictionary["Bug"])
+
+print("############################### \n \n")
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
