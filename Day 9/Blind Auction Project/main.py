9ca896116e8cc8bb7dc9bafb8ef66723cd85e7b4
George K <gkurkjian@myseneca.ca>
2024-10-11 19:35:47 -0400

    making the psedo code by using draw.io to analyze the code.

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,10 @@
+import art
+the_logo = art.logo
 # TODO-1: Ask the user for input
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+print(the_logo)
+# print("\n"* 20)  ## This will print spaces between the line
 
