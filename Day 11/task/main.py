fb0f19ed842c8b3464e5a9869341fae697960afc
George K <gkurkjian@myseneca.ca>
2024-10-23 17:18:28 -0400

    analyzing half-way through of the code

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,24 +1,37 @@
 from art import logo
 import random
 
-game_on = True
-
-
-while game_on:
-    user_input = input(f"Do you want to play a game of Blackjack? Type 'y' or 'n':  ")
+def deal_card():
+    """Returns a random card from a deck"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
-    if user_input == 'y':
-        print(logo)
+def calculate_score(cards):
+    ## Or short way you can right
+    ##if sum(cards) == 21 and len(cards) == 2:
+    if 11 in cards and 10 in cards and len(cards) == 2:
+        return 0
 
-        ## User's first hand will be given Two cards
-        user_cards = [random.choice(cards) for _ in range(2)]
-        user_num = sum(user_cards)
+    ## Step 8 to handle BlackJack
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
 
-        ## Computer first hand will be give One card
-        computer_card = [random.choice(cards)]
+    return sum(cards)
 
-        print(f"Your cards: [{user_cards[0]},{user_cards[1]}], current score: {user_num}")
-        print(f"Computer's first card: {computer_card}")
+if_game_over = False
 
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"Your cards: {user_cards}, current score: {user_score}")
+print(f"Computer's first card: {computer_cards[0]}")
 
