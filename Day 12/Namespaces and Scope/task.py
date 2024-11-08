03d08d47860ea35761a4cec1b6481f621da7b089
George K <gkurkjian@myseneca.ca>
2024-11-07 19:31:28 -0500

    First commit: Namespces and Scope Global Varibale priactice done

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -1,9 +1,13 @@
-enemies = 1
+## Global Scope
+enemies = 10
 
 
 def increase_enemies():
-    enemies = 2
-    print(f"enemies inside function: {enemies}")
+    global enemies
+    print(f"Unmodified Global enemies inside function(): {enemies}")
+    ## Local Scope
+    enemies += 1
+    print(f"Modified Global enemies inside function(): {enemies}")
 
 
 increase_enemies()
