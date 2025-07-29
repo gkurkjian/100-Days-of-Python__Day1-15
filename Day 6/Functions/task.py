cd6ffdf771e9ff60c294819d4a04d4ac9d8c8f3c
George K <gkurkjian@myseneca.ca>
2025-07-29 18:42:26 -0400

    Day 5 revising

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,14 @@
+## 1. regular function
+# def greet():
+#     print("hello")
+#     print("hello")
+#     print("hello")
+#
+# greet()
+
+## 2. Function that allows inputs
+def greet_with_name(name):
+    print(f"hello {name}")
+    print(f"how do you do {name}?")
+
+greet_with_name("George")
